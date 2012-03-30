e8f752b99c994df4b0c8e8dd16a0e1e4daf6e743
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2012-03-29 21:00:10 -0500

    Print a warning to stderr if no configuration files are found.

--- configuration.py
+++ configuration.py
@@ -1,9 +1,10 @@
 
+import sys
 import os
 import pwd
 import ConfigParser
 
-# TODO: there is no reason for these to be exclusive to pbs_map
+
 numnodes=1
 numprocs=1
 clients_per_pbs=1
@@ -12,23 +13,30 @@ walltime=None
 
 max_submissions=200
 
-from_address=None
-sendto_email_address=None
-
 def get_user_name():
     return pwd.getpwuid(os.getuid())[0]
+
+sendto_email_address=get_user_name() + '@localhost'
+from_address=get_user_name() + '@localhost'
 
 def config_email(configuration):
     global from_address, sendto_email_address
 
+    section = 'PBSUTIL' 
+
     # default to user@localhost
-    sendto_email_address=get_user_name() + '@localhost'
-    from_address=get_user_name() + '@localhost'
+    if configuration.has_section(section):
+        if configuration.has_option(section, 'sendto'):
+            sendto_email_address=configuration.get(section, 'sendto')
+
+        if configuration.has_option(section, 'sendfrom'):
+            from_email_address=configuration.get(section, 'sendfrom')
+
 
 def config_pbs(configuration):
     global numnodes, numprocs, clients_per_pbs, queue, walltime, max_submissions
 
-    section = 'PBSUTIL'  # configuration section for pyamber module.
+    section = 'PBSUTIL' 
 
     if configuration.has_section(section):
         if configuration.has_option(section, 'numnodes'):
@@ -50,14 +58,16 @@ def config_pbs(configuration):
         if configuration.has_option(section, 'max_submissions'):
             max_submissions = configuration.getint(section, 'max_submissions')
 
-
     
 
 def config(config_file_name):
     """Configure pyamber using the config parser."""
+    global some_config_file_exists
 
     if not os.path.exists(config_file_name):
         return
+
+    some_config_file_exists = True
 
     configuration = ConfigParser.SafeConfigParser()
     configuration.read(config_file_name)
@@ -68,6 +78,7 @@ def config(config_file_name):
 
 
 
+some_config_file_exists = False
 
 default_config_file_name = os.getenv('HOME') + '/.pbs_util.ini'
 config(default_config_file_name)
@@ -78,3 +89,5 @@ config(local_config_file_name)
 default_email_config_file_name = os.getenv('HOME') + '/.send_email.ini'
 config(default_email_config_file_name)
 
+if not some_config_file_exists:
+    sys.stderr.write("WARNING: Neither ~/.pyp_util.ini nor ./pbs_util.ini exist.")
